26cde32a6e9a8dbf41f1fee469e325cfe4d3e0ef
Luc Boissonneault <luc.boissonneault@outlook.com>
2017-07-14 16:16:22 -0400

    Test commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace VSCodeCSharpTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
