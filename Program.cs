08b01b63dc0044edaf7dce274cf68a61ba5ff66d
Luc Boissonneault <luc.boissonneault@outlook.com>
2017-07-14 15:48:39 -0400

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCodeCSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
